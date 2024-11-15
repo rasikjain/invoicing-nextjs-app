ef84b3e529cea49de625beb4c261b8091c597f02
Rasik Jain <jainrasik@gmail.com>
2024-11-15 21:21:30 +0530

    update invoice status

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
 import { db } from "@/db";
-import { Invoices } from "@/db/schema";
+import { Invoices, Status } from "@/db/schema";
 import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function createInvoiceAction(formData: FormData) {
@@ -24,4 +26,23 @@ export async function createInvoiceAction(formData: FormData) {
     })
 
     redirect(`/invoices/${results[0].id}`)
+}
+
+
+export const updateStatusAction = async (formData: FormData) => {
+    const { userId } = await auth();
+
+
+    if (!userId) {
+        return;
+    }
+
+    const id = formData.get('id') as string;
+    const status = formData.get('status') as Status;
+
+    const results = await db.update(Invoices)
+        .set({ status })
+        .where(and(eq(Invoices.id, parseInt(id)), eq(Invoices.userId, userId)))
+
+    revalidatePath(`/invoices/${id}`, 'page')
 }
---
+++ src/data/invoices.ts
@@ -0,0 +1,6 @@
+export const AVAILABLE_STATUSES = [
+    { id: 'open', label: 'Open' },
+    { id: 'paid', label: 'Paid' },
+    { id: 'void', label: 'Void' },
+    { id: 'uncollectible', label: 'Uncollectible' },
+];
